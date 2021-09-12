2dc4ea16b0cf076810322a6b6d0ba58b0d0a35d2
uni <77507520+Nan0006@users.noreply.github.com>
2021-09-12 20:29:33 -0300

    Add files via upload

---
+++ Meubot/lib/figurinhas.js
@@ -0,0 +1,20 @@
+const figurinhas = (pushname, prefix, sender, client, process) => { 
+	return `
+🕋 *Menu De Figurinhas* 🕋
+ 
+*══════◄••❀••►══════*
+
+│• 🍥 ${prefix}🐯 ( _fazer Figurinha_ ) 1 Credito
+    │
+│• 🍥 ${prefix}gift ( _O Comando + alguma palavra_) 1 Credito
+    │
+│• 🍥 ${prefix}gifanime 1 Credito
+    │
+│• 🍥 ${prefix}gifneko 1 Credito
+
+*══════◄••❀••►══════*
+
+*By: Nando.* 🕌 `
+}
+
+exports.figurinhas = figurinhas
---
+++ Meubot/lib/picture.js
@@ -0,0 +1,34 @@
+const picture = (pushname, prefix, sender, client, process) => { 
+	return `
+🕋 *Menu De Imagens & Edite* 🕋
+ 
+*══════◄••❀••►══════*
+
+│• 🎯 ${prefix}neko 1 Credito
+    │
+│• 🎯 ${prefix}anime 1 Credito
+    │
+│• 🎯 ${prefix}garotas 1 Credito 
+    │
+│• 🎯 ${prefix}perfilgirl 1 Credito
+    │
+│• 🎯 ${prefix}cachorro 1 Credito
+    │
+│• 🎯 ${prefix}google ( _Nome do personagem na frente_ ) 1 Credito
+    │
+│• 🎯 ${prefix}blackpink 1 Credito
+    │
+│• 🎯 ${prefix}gato 1 Credito
+    │
+│• 🎯 ${prefix}ero 1 Credito
+    │
+│• 🎯 ${prefix}eroneko 1 Credito
+    │
+│• 🎯 ${prefix}erogirl 1 Credito
+
+*══════◄••❀••►══════*
+
+*By: Nando.* 🕌 `
+}
+
+exports.picture = picture
